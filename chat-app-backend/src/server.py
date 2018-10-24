78b7c82a0617b1a559bfebdd457ce69e01adc402
liuleen <ruchenliu447@gmail.com>
2018-10-24 14:47:43 -0700

    part2 done

--- chat-app-backend/src/server.py
+++ chat-app-backend/src/server.py
@@ -1,13 +1,30 @@
-from flask import Flask
+from flask import (
+    Flask,
+    request,
+    abort, 
+    jsonify
+)
 
 app = Flask(__name__)
 
+users = []
 
 @app.route("/")
 def hello():
     return "hello\n"
 
-
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get('username', None)
+    user_taken = username in users
+    if username is None or user_taken:
+        abort(401)
+    else:
+        users.append(username)
+        return jsonify({
+        'status': 'ok',
+        'message': 'Successfully Logged in',
+        })
 
 if __name__ == '__main__':
     app.run(debug=True)
