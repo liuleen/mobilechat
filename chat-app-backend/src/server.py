594ed70fd4e4c2c90696d14c09b8e39ae30ea06f
liuleen <ruchenliu447@gmail.com>
2018-10-25 02:09:15 -0700

    new send route

--- chat-app-backend/src/server.py
+++ chat-app-backend/src/server.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import (
     Flask,
     request,
@@ -28,6 +29,13 @@ def login():
         'status': 'ok',
         'message': 'Successfully Logged in',
         })
+
+@app.route("/send", methods=["POST"])
+def send():
+    username = request.json.get('username', None)
+    message = request.json.get('message', None)
+    timestamp = datetime.now()
+    id = 
 
 if __name__ == '__main__':
     app.run(debug=True)
