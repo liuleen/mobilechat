fd7a3fcb06b587044fa962558b6c9eeb92d5b2d0
liuleen <ruchenliu447@gmail.com>
2018-10-25 14:39:19 -0700

    create message dictionary

--- chat-app-backend/src/server.py
+++ chat-app-backend/src/server.py
@@ -1,3 +1,4 @@
+import uuid
 from datetime import datetime
 from flask import (
     Flask,
@@ -10,6 +11,9 @@ app = Flask(__name__)
 
 #global variable users to store usernames
 users = []
+
+#create a dict to store messages
+messages = dict()
 
 @app.route("/")
 def hello():
@@ -30,12 +34,23 @@ def login():
         'message': 'Successfully Logged in',
         })
 
+# 3 variables -> username, message, timestamp
+# generate a unique id for the message
+# map each id to a specific id (use id as the key) -> which then gets added to our dictionary above
+
 @app.route("/send", methods=["POST"])
 def send():
     username = request.json.get('username', None)
     message = request.json.get('message', None)
     timestamp = datetime.now()
-    id = 
+    id = uuid.uuid4()
+
+    messages[id] = {
+        'username': username,
+        'message': message,
+        'timestamp': timestamp,
+        'id': id, 
+    }
 
 if __name__ == '__main__':
     app.run(debug=True)
